2b72a19c6bd9d38507ecd2dc4932758690ba7575
lyerlype1501 <143443383+lyerlype1501@users.noreply.github.com>
2023-09-20 14:45:16 -0400

    First Commit
    Simulated a travel budget.

---
+++ P1HW2_LyerlyPinaEmmanuel.py
@@ -0,0 +1,30 @@
+Python 3.8.5 (default, Sep  3 2020, 21:29:08) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/lyerlype1501/Downloads/P1HW2_LyerlyPinaEmmanuel.py =====
+This program calculates and displays travel expenses
+Enter Budget:
+Traceback (most recent call last):
+  File "C:/Users/lyerlype1501/Downloads/P1HW2_LyerlyPinaEmmanuel.py", line 2, in <module>
+    budget = int(input('Enter Budget:'))
+ValueError: invalid literal for int() with base 10: ''
+>>> 
+===== RESTART: C:/Users/lyerlype1501/Downloads/P1HW2_LyerlyPinaEmmanuel.py =====
+This program calculates and displays travel expenses
+Enter Budget:1000
+Enter your travel destination:Florida
+How much will you spend on gas?50
+How much will you spend on hotel?200
+How much will you spend on food?80
+------------Travel Expenses------------
+Location: Florida
+Initial Budget: 1000
+
+
+Fuel: 50
+Accomodation: 200
+Food: 80
+
+
+Remaining Balance: 670
+>>> 
