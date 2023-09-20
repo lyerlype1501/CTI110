64b46040d3178b8d150a9a039ac6c04d19efefb2
lyerlype1501 <143443383+lyerlype1501@users.noreply.github.com>
2023-09-20 14:44:13 -0400

    First Commit
    Basic output with variables

---
+++ P1HW1_LyerlyPinaEmmanuel.py
@@ -0,0 +1,14 @@
+Python 3.8.5 (default, Sep  3 2020, 21:29:08) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:/Users/lyerlype1501/Downloads/P1HW1_LyerlyEmmanuel.py =======
+Enter integer:
+4
+You entered: 4
+4 squared is 16
+And 4 cubed is 64
+Enter another integer:5
+5
+4 + 5 is 9
+4 * 5 is 20
+>>> 
